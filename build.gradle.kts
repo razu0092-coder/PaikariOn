48f0364cc29931f48a02aab49ffbb5045bf0e45c
razu0092-coder <razu0092@gmail.com>
2026-07-29 17:51:07 +0600

    Update Gradle plugins to newer versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file
 plugins {
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.kotlin.compose) apply false
-  alias(libs.plugins.google.devtools.ksp) apply false
-  alias(libs.plugins.roborazzi) apply false
-  alias(libs.plugins.secrets) apply false
-  alias(libs.plugins.google.services) apply false
+  id("com.android.application") version "8.2.2" apply false
+  id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+  id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+  id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+  id("com.google.gms.google-services") version "4.4.1" apply false
 }
